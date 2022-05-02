226297895b7460836451f211967014dbc9bb163a
Bdotwall <brwall92@gmail.com>
2022-05-02 13:26:15 -0400

    Printing Exercises 1st Commit

---
+++ HiFriend/src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String s="Hi " + "Larry Sprinkle";
+        System.out.print(s);
+    }
+}
---
+++ MyNameIs/src/MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My name is");
+        System.out.println("Brandon");
+        System.out.println("Wall");
+    }
+}
